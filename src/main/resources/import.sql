6d9d2587dea1ed57376c8b8074542f556d843f4a
Diego Alves <djalves424@gmail.com>
2024-01-22 10:07:13 -0300

    Bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$ZV6Lk.2OlBvxWqRNDNNxfOtiDtazo..ivRcVck98XbgDE0sXJcStK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$ZV6Lk.2OlBvxWqRNDNNxfOtiDtazo..ivRcVck98XbgDE0sXJcStK');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
